0c5ca48d51b917d538f2841ec75a2eba986137f2
unknown <ahatasamul15-3503@diu.edu.bd>
2023-05-28 19:01:00 +0600

    project setup completed

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ashar Alo Shool</title>
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+<body class="back">
+  
+    <img src="./img/cover.jpg" alt="Cover" width="800">
+    <div class="login">
+      <h1 class="heading">Admin Login</h1>
+      <div class="log">
+          <form action="" method="POST">
+            <label>Username</label>
+            <input type="text" name="name" required class="input"> <br>
+            <label>Password</label>
+            <input type="password" name="password" required class="input"> <br>
+            <button type="submit" class="btn" name="login">Login</button>
+          </form>
+      </div>
+    </div>
+  
+</body>
+</html>
